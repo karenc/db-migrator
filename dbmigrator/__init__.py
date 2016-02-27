decb9212a5e0adae31d8e7562fa8258c222aae23
karen chan <karen@karen-chan.com>
2016-02-27 22:43:26 +0100

    Add a logger for dbmigrator that writes to stdout
    For example in a migration file
    20160128111115_mimetype_removal_from_module_files.py:
    
    ```
    from dbmigrator import logger
    
    logger.info('message from migration')
    ```
    
    You will see this when you run the migration:
    
    ```
    [INFO] dbmigrator (20160128111115_mimetype_removal_from_module_files.py) - message from migration
    ```
    
    Close #3

---
+++ dbmigrator/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import sys
+
+logger = logging.getLogger('dbmigrator')
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter('[%(levelname)s] %(name)s (%(filename)s) - %(message)s'))
+logger.addHandler(handler)
